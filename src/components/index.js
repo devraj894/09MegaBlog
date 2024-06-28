ed3d302b36d85fcc76ccd0e8d98f7fda343a4e18
Devraj Songara <devrajsongara894@gmail.com>
2024-06-28 23:40:11 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container"
+import Logo from "./Logo"
+import Button from "./Button"
+import Input from "./Input"
+import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+// import Post from "../pages/Post"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Select from "./Select"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    Button,
+    Input,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    // Post,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Select,
+}
